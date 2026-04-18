939f818835a35791659e5327806875f2220d3e73
Wei Chen <chenweisomebody@gmail.com>
2026-04-18 23:37:36 +0800

    chore: 更新 SegmentKit xcframework 到 v1.1.0

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         // 预编译的 XCFramework（通过 GitHub Release 分发）
         .binaryTarget(
             name: "SegmentKit",
-            url: "https://github.com/chenweisomebody126/SegmentKit/releases/download/v1.0.0/SegmentKit.xcframework.zip",
-            checksum: "a537e4c8bb278e861480d9cf5bae66e8e39f8e89740a467c6dc405ccb5de3f5c"
+            url: "https://github.com/chenweisomebody126/SegmentKit/releases/download/v1.1.0/SegmentKit.xcframework.zip",
+            checksum: "e6ab4fb857a27248a2bb9536900a5891139252364208d35528917d00a7c839d0"
         ),
     ]
 )
